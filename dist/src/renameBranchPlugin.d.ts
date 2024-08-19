128ce2612682624deed6ede1a95668bb44bab1c1
Maxim Zavadskiy <maximzavadskiy@gmail.com>
2024-08-19 11:15:00 +0300

    chore: dist

--- dist/src/renameBranchPlugin.d.ts
+++ dist/src/renameBranchPlugin.d.ts
@@ -1,8 +1,10 @@
-import { GitHub, PluginFactoryOptions } from "release-please";
+import { Commit, GitHub, PluginFactoryOptions, Strategy } from "release-please";
 import { CandidateReleasePullRequest, RepositoryConfig } from "release-please/build/src/manifest";
 import { ManifestPlugin } from "release-please/build/src/plugin";
+import { Release } from "release-please/build/src/release";
 export declare class RenameBranches extends ManifestPlugin {
     constructor(github: GitHub, targetBranch: string, repositoryConfig: RepositoryConfig);
     run(candidates: CandidateReleasePullRequest[]): Promise<CandidateReleasePullRequest[]>;
+    preconfigure(strategiesByPath: Record<string, Strategy>, _commitsByPath: Record<string, Commit[]>, _releasesByPath: Record<string, Release>): Promise<Record<string, Strategy>>;
 }
 export declare const renameBranchesPluginBuilder: (options: PluginFactoryOptions) => RenameBranches;
